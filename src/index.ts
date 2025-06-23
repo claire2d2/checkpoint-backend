dd9683ff2168ec2565342dcc23dc4d3e44054d02
Claire <claireyuan.song@gmail.com>
2025-06-23 09:58:10 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = ``;
+const resolvers = {};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
